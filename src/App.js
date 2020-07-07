ecdc24e0542b36f75c92db3094cf298a3271e44a
karmaKiller3352 <alievsul@gmail.com>
2020-07-07 23:42:17 +0300

    Init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+import './App.scss';
+
+import butterfly from './assets/images/butterfly.gif';
+import flower from './assets/images/flower.png';
 
 function App() {
+  const [coordinatesBF, setCoordinatesBF] = useState({
+    x: '20px',
+    y: '20px',
+  });
+  const [coordinatesCS, setCoordinatesCS] = useState({
+    x: '20px',
+    y: '20px',
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className='container'
+      onMouseMove={({ clientX, clientY }) => {
+        setCoordinatesBF({
+          x: clientX - 75,
+          y: clientY - 75,
+        });
+        setCoordinatesCS({
+          x: clientX - 100,
+          y: clientY - 100,
+        });
+      }}
+    >
+      <img
+        style={{ left: coordinatesCS.x, top: coordinatesCS.y }}
+        className='cursor'
+        src={flower}
+        alt=''
+      />
+      <img
+        className='bf'
+        style={{ left: coordinatesBF.x, top: coordinatesBF.y }}
+        src={butterfly}
+        alt=''
+      />
     </div>
   );
 }

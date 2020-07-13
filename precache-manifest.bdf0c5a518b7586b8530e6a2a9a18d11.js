e853b89751b5f34e3b522b88839b46d4a4bbdd17
karmaKiller3352 <alievsul@gmail.com>
2020-07-14 01:02:21 +0300

    deploy

---
+++ precache-manifest.bdf0c5a518b7586b8530e6a2a9a18d11.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "690d5ec00e85c1e76afc393d70ea5ab2",
+    "url": "/animated/index.html"
+  },
+  {
+    "revision": "31d1721362a00e6492e6",
+    "url": "/animated/static/css/main.ad38bff0.chunk.css"
+  },
+  {
+    "revision": "1960a1e7039d746a0c90",
+    "url": "/animated/static/js/2.8da32cd9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/animated/static/js/2.8da32cd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31d1721362a00e6492e6",
+    "url": "/animated/static/js/main.a7dcac09.chunk.js"
+  },
+  {
+    "revision": "e053e48674dd36dbda0f",
+    "url": "/animated/static/js/runtime-main.ab53e3d4.js"
+  },
+  {
+    "revision": "e77ee236b35be08dfe5bce401eb9f04f",
+    "url": "/animated/static/media/butterfly.e77ee236.gif"
+  },
+  {
+    "revision": "732997255b5f50053ed415600b6514f3",
+    "url": "/animated/static/media/flower.73299725.png"
+  }
+]);
